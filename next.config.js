19b05cc2295dad060047c08dca6ca04ab4749c15
BytesPlatform <developer@bytesplatform.com>
2025-10-21 00:34:28 +0500

    build fixes3.0

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
     ignoreBuildErrors: false,
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   poweredByHeader: false,
   compress: true,
